6ee696f803c4ea306bb566bc0d070d78e867ab23
jacobrees <jacob.rees97@yahoo.com>
2021-06-11 14:24:57 +0100

    Add functionality so user can only open one todo options pannel at one time

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const todoMenuBtns = document.querySelectorAll('.todo-menu-btn');
 todoMenuBtns.forEach((todoMenuBtn) => {
   todoMenuBtn.addEventListener('click', (e) => {
     const todoOptions = e.currentTarget.parentElement.parentElement.parentElement.childNodes[3];
-    todoOptions.classList.toggle('show-todo-options');
+    if ([].slice.apply(todoOptions.classList).includes('show-todo-options')) {
+      todoOptions.classList.remove('show-todo-options');
+    } else {
+      const allTodoOptions = todoMenuBtns.forEach((todoOptions) => {
+        todoOptions.parentElement.parentElement.parentElement.childNodes[3].classList.remove('show-todo-options');
+      });
+      todoOptions.classList.toggle('show-todo-options');
+    }
   });
 });
