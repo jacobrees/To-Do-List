63ed8b83c014cc0df8cdf99d14e88a827830db5c
jacobrees <jacob.rees97@yahoo.com>
2021-06-11 14:08:09 +0100

    Add ability to toggle todo menu options

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './scss/styles.scss';
-import './scss/checkbox.scss'
+import './scss/checkbox.scss';
+
+const todoMenuBtns = document.querySelectorAll('.todo-menu-btn');
+
+todoMenuBtns.forEach((todoMenuBtn) => {
+  todoMenuBtn.addEventListener('click', (e) => {
+    const todoOptions = e.currentTarget.parentElement.parentElement.parentElement.childNodes[3];
+    todoOptions.classList.toggle('show-todo-options');
+  });
+});
