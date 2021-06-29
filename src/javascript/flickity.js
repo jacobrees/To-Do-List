669c1134f91e4353bb44ad1751d05ebf0d0165e1
jacobrees <jacob.rees97@yahoo.com>
2021-06-29 23:23:38 +0100

    Break up and organise code from index.js

---
+++ src/javascript/flickity.js
@@ -0,0 +1,11 @@
+const carousel = document.querySelector('.categories-container');
+const flkty = new Flickity(carousel, { // eslint-disable-line
+  contain: false,
+  freeScroll: true,
+  watchCSS: true,
+  cellAlign: 'center',
+  prevNextButtons: true,
+  pageDots: false,
+});
+
+export default flkty;
