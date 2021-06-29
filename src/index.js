a05facaec08e73b4276da91cfd01076195b25613
jacobrees <jacob.rees97@yahoo.com>
2021-06-29 12:11:28 +0100

    Add functionality where todo done box crosses todo out if ticked

--- src/index.js
+++ src/index.js
@@ -143,3 +143,14 @@ sortCategoryBtns.forEach(((sortCategoryBtn) => {
     }
   });
 }));
+
+const doneCheckboxes = document.querySelectorAll('.done-checkbox');
+
+doneCheckboxes.forEach(((doneCheckbox) => {
+  doneCheckbox.addEventListener('change', (e) => {
+    const currentTodoHeading = e.currentTarget.parentElement.parentElement.parentElement.childNodes[1]; // eslint-disable-line
+    const currentTodoDescription = e.currentTarget.parentElement.parentElement.parentElement.childNodes[3]; // eslint-disable-line
+    currentTodoHeading.classList.toggle('done');
+    currentTodoDescription.classList.toggle('done');
+  });
+}));
