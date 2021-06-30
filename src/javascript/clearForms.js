91758f847b1a7ddafdcfef95705cc67df39643c4
jacobrees <jacob.rees97@yahoo.com>
2021-06-30 01:17:59 +0100

    Remove event listeners from clear forms functions

--- src/javascript/clearForms.js
+++ src/javascript/clearForms.js
@@ -1,48 +1,33 @@
 const clearEditTodoForm = () => {
-  const closeEditTodoBtns = document.querySelectorAll('.cancel-edit-existing-todo-button, .add-edit-existing-todo-button');
   const editTodoInputs = document.querySelectorAll('#edit-todo-title, #edit-todo-description');
 
-  closeEditTodoBtns.forEach((closeEditTodoBtn) => {
-    closeEditTodoBtn.addEventListener('click', () => {
-      setTimeout(() => {
-        editTodoInputs.forEach((todoInput) => {
-          todoInput.value = '';
-        });
-      }, 250);
+  setTimeout(() => {
+    editTodoInputs.forEach((todoInput) => {
+      todoInput.value = '';
     });
-  });
+  }, 250);
 };
 
 const clearAddTodoForm = () => {
-  const closeAddTodoBtns = document.querySelectorAll('.cancel-add-new-todo-button, .add-add-new-todo-button');
   const addTodoInputs = document.querySelectorAll('#add-todo-title, #add-todo-description');
   const addCategoryBtns = document.querySelectorAll('.category-btn-add');
 
-  closeAddTodoBtns.forEach((closeAddTodoBtn) => {
-    closeAddTodoBtn.addEventListener('click', () => {
-      setTimeout(() => {
-        addCategoryBtns.forEach((addCategoryBtn) => {
-          addCategoryBtn.classList.remove('active-category');
-        });
-        addTodoInputs.forEach((todoInput) => {
-          todoInput.value = '';
-        });
-      }, 250);
+  setTimeout(() => {
+    addCategoryBtns.forEach((addCategoryBtn) => {
+      addCategoryBtn.classList.remove('active-category');
     });
-  });
+    addTodoInputs.forEach((todoInput) => {
+      todoInput.value = '';
+    });
+  }, 250);
 };
 
 const clearAddCategoryForm = () => {
-  const closeAddCategoryBtns = document.querySelectorAll('.cancel-add-category-button, .add-add-category-button');
   const addCategoryInput = document.querySelector('#add-category-title');
 
-  closeAddCategoryBtns.forEach((closeAddCategoryBtn) => {
-    closeAddCategoryBtn.addEventListener('click', () => {
-      setTimeout(() => {
-        addCategoryInput.value = '';
-      }, 250);
-    });
-  });
+  setTimeout(() => {
+    addCategoryInput.value = '';
+  }, 250);
 };
 
 const setClearForms = () => {
