04dbe80c6407c15a377a9540b8d96941f5246cfe
jacobrees <jacob.rees97@yahoo.com>
2021-07-27 00:36:34 +0100

    Add exception handing to add categories

--- src/javascript/categoriesController.js
+++ src/javascript/categoriesController.js
@@ -1,13 +1,15 @@
 import { categoriesArray, setCategoriesArray, deleteAllTodosInCategory } from './database.js';
 import { toggleLoadingScreen } from './startScreen.js';
 import { formatCategoryTitle } from './formatText.js';
+import { flashError } from './flashError.js';
+
+const firstNull = categoriesArray.findIndex((category) => category.category === null);
 
 const addCategory = (category) => {
   const createCategory = (category) => ({ category });
-  const firstNull = categoriesArray.findIndex((category) => category.category === null);
 
   if (firstNull !== -1) {
-    const newCategory = createCategory(formatCategoryTitle(category));
+    const newCategory = createCategory(category);
     setCategoriesArray(firstNull, newCategory);
     toggleLoadingScreen();
     setTimeout(() => { window.location.reload(); }, 550);
@@ -18,8 +20,22 @@ const setAddCategoryBtn = () => {
   const addCategoryBtn = document.querySelector('.add-add-category-button');
   const categoryNameInput = document.querySelector('#add-category-title');
   addCategoryBtn.addEventListener('click', () => {
-    const categoryName = categoryNameInput.value;
-    addCategory(categoryName);
+    const categoryName = formatCategoryTitle(categoryNameInput.value);
+    if (firstNull === -1) {
+      flashError('You Cannot Have More Than 4 Categories');
+    } else if (categoriesArray.some((category) => category.category === categoryName)) {
+      flashError('Category Cannot Have Same Name As Existing Category');
+    } else if (categoryName.includes(' ')) {
+      flashError('Category Name Cannot Contain Spaces Between Letters');
+    } else if (categoryName.length < 3) {
+      flashError('Category Name Cannot Be Less Than 3 Letters Long');
+    } else if (categoryName.length > 15) {
+      flashError('Category Name Cannot Be Greater Than 15 Letters Long');
+    } else if (/[^a-z]/i.test(categoryName)) {
+      flashError('Category Name Can Only Contain Letters');
+    } else {
+      addCategory(categoryName);
+    }
   });
 };
 
