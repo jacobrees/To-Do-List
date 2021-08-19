d0144188c045c5c7d8ce996e8a5f4b679b6cd944
jacobrees <jacob.rees97@yahoo.com>
2021-08-19 08:56:15 +0100

    Add iso.arrange method to startApp function

--- src/javascript/startApp.js
+++ src/javascript/startApp.js
@@ -22,6 +22,7 @@ const start = () => {
   setTodosController();
   displayAllTodos();
   window.addEventListener('load', () => {
+    iso.arrange({ filter: '*' });
     setTimeout(() => {
       toggleLoadingScreen();
     }, 1800);
